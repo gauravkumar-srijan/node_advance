d1d716797c3894b2b74a52a5bef3dd383b0e249c
GauravKumarmishra <gaurav.kumar@srijan.net>
2023-08-12 02:13:29 +0530

    first commit

---
+++ index.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+const app = express();
+
+
+app.listen(process.env.PORT || 8080 , ()=>{
+    console.log('listening on port', process.env.PORT);
+});
